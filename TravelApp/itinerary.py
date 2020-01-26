72432870720258a1ae1a4909e270eacfea6e1bdf
ArnavParekhji <41626778+ArnavParekhji@users.noreply.github.com>
2020-01-26 15:17:25 +0530

    Update itinerary.py

--- TravelApp/itinerary.py
+++ TravelApp/itinerary.py
@@ -2,36 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import math
+import twitter
 
-def gen_it(data, days):
+def gen_it(data, senti, days):
     num_places = days*3
     time = ["Morning", "Afternoon", "Evening"]
     places = []
     price = []
-    for key in data.keys():
-        places.append(key)
+    sent = []
+    for key in senti:
+        places.append(key[0])
     if num_places < len(data):
         places = places[:num_places]
     for p in places:
         price.append(data[p])
+        for k,v in senti:
+            if k==p:
+                sent.append(v)
 
     out = []
     for i in range(0, len(places)):
-        out.append((time[i%3], places[i], price[i]))
+        out.append((time[i%3], places[i], price[i], sent[i]))
     return out
 
 
 def get_places(term, days):
     data = {}
+    senti = {}
     url = "https://www.fabhotels.com/blog/places-to-visit-in-{}/".format(term)
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     for i in soup.find_all('h3'):
         if i.text[0] is "T":
             break
+        senti[i.text] = twitter.getSentiments(i.text.split('.')[1])
         data[i.text] = int(math.ceil(random.randint(30,300) / 10)) * 10
-
-    return gen_it(data, days)
+    senti = sorted(senti.items(), key = lambda kv:(kv[1]), reverse=True)
+    return gen_it(data, senti, days)
 
 
 def get_it(term, days):
@@ -50,3 +57,4 @@ def get_it(term, days):
                 td = tr.find_all('td')
                 row = [i.text for i in td]
                 print(row)
+
